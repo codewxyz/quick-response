55dec969370a22159f5379d3e6eb27cd0babb5d5
codewxyz <codew.xyz@gmail.com>
2017-09-10 22:33:20 +0700

    routing for screens

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var app = require('express')();
 var http = require('http').Server(app);
 var nunjucks = require('nunjucks');
 var io = require('socket.io')(http);
-var serveStatic = require('serve-static');
+var serveStatic = require('serve-static');//get static file
+var bodyParser = require('body-parser');//read data from post method
 
 //set public directory for static files (css,js)
 app.use(serveStatic('public', { 'index': false }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 //set up template engine for nunjucks
 nunjucks.configure('views', {
@@ -15,7 +19,23 @@ nunjucks.configure('views', {
 
 //---------------routing-----------------
 app.get('/', function(req, res) {
-    res.render('main.html', { name: 'hungtp', age: '25' });
+    // res.render('main.html', { name: 'hungtp', age: '25' });
+    res.redirect('/login');
+});
+app.get('/register', function(req, res) {
+    res.render('register.html');
+});
+app.get('/login', function(req, res) {
+    res.render('login.html');
+});
+app.post('/login', function(req, res) {
+    console.log(req.body);
+
+    if (!req.body) return res.sendStatus(400);
+    res.send('welcome, ' + req.body.username);
+});
+app.get('/main', function(req, res) {
+    res.render('chat_room.html');
 });
 
 //handling socket.io
@@ -26,10 +46,29 @@ io.on('connection', function(socket) {
         console.log('user disconnected');
     });
     socket.on('chat message', function(msg) {
-    	console.log(msg);
+        console.log(msg);
         io.emit('chat message', msg);
     });
 })
+
+//socket for ttm
+var ttm = io.of('/ttm-chat');
+ttm.on('connection', function(socket) {
+    console.log('someone connected to ttm');
+    socket.join('tinh tinh', () => {
+        let rooms = Object.keys(socket.rooms);
+        console.log(rooms); // [ <socket.id>, 'room 237' ]
+    });
+
+    socket.in('tinh tinh').on('chat message', function(msg) {
+        console.log('ttm room tinh tinh ' + msg);
+        ttm.in('tinh tinh').emit('chat message', msg);
+    });
+
+    console.log('socket id ' + socket.id);
+    console.log('rooms ');
+    console.log(socket.rooms);
+});
 
 //server listen
 http.listen(3000, function() {
